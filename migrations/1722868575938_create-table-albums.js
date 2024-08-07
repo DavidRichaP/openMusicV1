9cf77b81d97f07700cd2bbafa3b9a5a277129af8
david <davidrichakerja@gmail.com>
2024-08-07 23:42:35 +0700

    added postgres migration files

---
+++ migrations/1722868575938_create-table-albums.js
@@ -0,0 +1,44 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.createTable('albums', {
+        albumId: {
+            type:'VARCHAR(50)',
+            primaryKey: true,
+            notNull: true,
+        },
+        name: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        year: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        created_at: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        updated_at: {
+            type: 'TEXT',
+            notNull: true,
+        },
+    })
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropTable('albums');
+};
