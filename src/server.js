e7dba92706a86f8587a327ca8a73554d344dacfd
david <davidrichakerja@gmail.com>
2024-08-21 23:50:51 +0700

    fixes on API registry

--- src/server.js
+++ src/server.js
@@ -1,50 +1,52 @@
-const Hapi = require('@hapi/hapi');
-const albumService = require('./services/postgres/AlbumService');
-const musicService = require('./services/postgres/SongService');
-const songValidator = require('../src/validator/songs');
-const albumValidator = require('../src/validator/album');
-require('dotenv').config();
+const Hapi = require('@hapi/hapi')
+const albumService = require('./services/postgres/AlbumService')
+const musicService = require('./services/postgres/SongService')
+const songsValidator = require('../src/validator/songs')
+const albumValidator = require('../src/validator/album')
+const albums = require('./api/albums')
+const songs = require('./api/songs')
+require('dotenv').config()
 
 const init = async () => {
-    const openMusicSongs = new musicService();
-    const openMusicAlbums = new albumService();
-    const server = Hapi.server({
-        port: process.env.PORT,
-        host: process.env.HOST,
-        routes: {
-            cors:{
-                origin: ['*'],
-            },
-        },
-    });
+	const openMusicSongs = new musicService()
+	const openMusicAlbums = new albumService()
+	const server = Hapi.server({
+		port: process.env.PORT,
+		host: process.env.HOST,
+		routes: {
+			cors: {
+				origin: ['*'],
+			},
+		},
+	})
 
-    await server.register([
-        {
-            plugin: albums,
-            options: {
-                service: {
-                    album: openMusicAlbums
-                },
-                validator: {
-                    album: albumValidator
-                },
-            },
-        },
-        {
-            plugin: songs,
-            options: {
-                service: {
-                    song: openMusicSongs
-                },
-            },
-            validator: {
-                song: songValidator
-            },
-        },
-    ]);
+	await server.register([
+		{
+			plugin: albums,
+			options: {
+				service: {
+					album: openMusicAlbums,
+				},
+				validator: {
+					album: albumValidator,
+				},
+			},
+		},
+		{
+			plugin: songs,
+			options: {
+				service: {
+					song: openMusicSongs,
+				},
+				validator: {
+					song: songsValidator,
+				},
+			},
+		},
+	])
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
-};
+	await server.start()
+	console.log(`Server berjalan pada ${server.info.uri}`)
+}
 
-init();
+init()
